d9dbd82e3d11afdc8262c1f637236aa7664b17b0
teten <teten.nugraha18@gmail.com>
2018-05-25 11:29:33 +0700

    update

---
+++ src/main/java/com/tens/spring/blog/SpringBootBlogExerciseApplication.java
@@ -0,0 +1,12 @@
+package com.tens.spring.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBlogExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBlogExerciseApplication.class, args);
+	}
+}
